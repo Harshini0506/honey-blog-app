c1ae3b46752d35c17c0cfb04fbb98c9c5f56e4f9
Harshini0506 <153800265+Harshini0506@users.noreply.github.com>
2024-06-21 20:29:19 +0530

    First Commit

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='articleHome'>
+      <h1 style={{color:'#191970', margin:'12px'}}> Wisdom unlocks the secret of life</h1>
+      <img src="https://tse4.mm.bing.net/th?id=OIP._CnjCvpahVaD0W24ozj2-AHaE8&pid=Api&P=0&h=180" alt="" className="artcleImage" />
+      <p style={{ fontFamily: 'serif' , paddingTop:'40px' }} className="lead ">
+      Knowledge is the gateway to understanding the world around us. It empowers individuals to navigate complexities with clarity and insight. Through  and insightlearning Knowledge  rough  and insightlearning  shapes, innovates, challenges, evolves, enriches, transforms, inspires.and experience, we unravel mysteries, solve problems, and innovate. Knowledge fosters growth, enabling us to make informed decisions and contribute meaningfully to society. It transcends boundaries, connecting diverse perspectives and cultures. Embracing knowledge is a lifelong journey our  lives and shapes a lives and shapes a  future.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
